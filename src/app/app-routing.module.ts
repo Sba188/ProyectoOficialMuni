1d009c6196e8bfcf228b8bf9b0c1a8736644f272
Sba188 <Sebad2607@gmail.com>
2024-10-08 11:59:52 -0300

    ahora si?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GruposComponent } from './modules/admin/grupos/grupos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",component:GruposComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminModule } from './modules/admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AdminModule,
+    HttpClientModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
